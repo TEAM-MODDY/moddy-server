638845c04cd34784921d4c368f5a8de2cf8ae9b4
KWY <wodrk1001@gmail.com>
2024-01-03 21:23:23 +0900

    #3 [feat] wrapper error/success response 코드 생성

---
+++ src/main/java/com/modee/server/common/dto/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.modee.server.common.dto;
+
+import com.modee.server.common.exception.enums.ErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+    private final int code;
+    private final String message;
+
+    public static ErrorResponse error(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.getHttpStatus().value(), errorCode.getMessage());
+    }
+}
