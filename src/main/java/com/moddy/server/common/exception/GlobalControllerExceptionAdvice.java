5ada5f2ef97a437794d7b93f151082cac84d3f7d
KWY <wodrk1001@gmail.com>
2024-01-07 02:57:42 +0900

    #25 [feat] 공통 controller advice 설정

--- src/main/java/com/moddy/server/common/exception/GlobalControllerExceptionAdvice.java
+++ src/main/java/com/moddy/server/common/exception/GlobalControllerExceptionAdvice.java
@@ -1,7 +1,79 @@
 package com.moddy.server.common.exception;
 
+import com.moddy.server.common.dto.ErrorResponse;
+import com.moddy.server.common.exception.model.BadRequestException;
+import com.moddy.server.common.exception.model.ConflictException;
+import com.moddy.server.common.exception.model.NotFoundException;
+import com.moddy.server.common.exception.model.UnAuthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.moddy.server.common.exception.enums.ErrorCode.INTERNAL_SERVER_EXCEPTION;
+import static com.moddy.server.common.exception.enums.ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerExceptionAdvice {
+    private final static String START_ERROR = "================================================NEW===============================================\n";
+
+    /**
+     * 400 Bad Request
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    protected ErrorResponse handleBadRequestException(final BadRequestException e) {
+        return ErrorResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 401 UnAuthorization
+     */
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UnAuthorizedException.class)
+    protected ErrorResponse handleUnAuthorizedException(final UnAuthorizedException e) {
+        return ErrorResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 404 Not Found
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    protected ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return ErrorResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 405 Method Not Allowed
+     * 지원하지 않은 HTTP method 호출 할 경우 발생하는 Exception
+     */
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException exception) {
+        return ErrorResponse.error(METHOD_NOT_ALLOWED_EXCEPTION);
+    }
+
+    /**
+     * 409 Conflict
+     */
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ConflictException.class)
+    protected ErrorResponse handleConflictException(final ConflictException e) {
+        return ErrorResponse.error(e.getErrorCode());
+    }
+
+    /**
+     * 500 Internal Server
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    protected ErrorResponse handleException(final Exception error) {
+        log.error(START_ERROR + error.getMessage(), error);
+        return ErrorResponse.error(INTERNAL_SERVER_EXCEPTION);
+    }
 }
--- src/main/java/com/moddy/server/common/exception/enums/ErrorCode.java
+++ src/main/java/com/moddy/server/common/exception/enums/ErrorCode.java
@@ -7,6 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    /**
+     * 405 METHOD_NOT_ALLOWED
+     */
+    METHOD_NOT_ALLOWED_EXCEPTION(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드 입니다."),
+
     INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private final HttpStatus httpStatus;
