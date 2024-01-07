c333f88fe39a0550f3d84c9b57039fb450e56d63
KWY <wodrk1001@gmail.com>
2024-01-08 03:27:59 +0900

    #39 [feat] handler method argument resolver 구현

--- src/main/java/com/moddy/server/common/exception/enums/ErrorCode.java
+++ src/main/java/com/moddy/server/common/exception/enums/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    // 400
+    INVALID_TOKEN_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰을 입력했습니다."),
+
     // 401
+    TOKEN_NOT_CONTAINED_EXCEPTION(HttpStatus.UNAUTHORIZED, "Access Token이 필요합니다."),
     TOKEN_TIME_EXPIRED_EXCEPTION(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다. 다시 로그인 해주세요."),
 
     // 500
