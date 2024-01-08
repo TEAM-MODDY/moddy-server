c2fb3d55f597c1463bbf94d7e27eed7f04aa1088
hellozo0 <ahju0011@gmail.com>
2024-01-09 04:22:41 +0900

    #35 [feat] PageInfo 삭제 후 Response 변경

--- src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
+++ src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
@@ -1,6 +1,5 @@
 package com.moddy.server.controller.model.dto.response;
 
-import com.moddy.server.common.dto.PageInfo;
 import com.moddy.server.domain.model.ModelApplyStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -8,7 +7,8 @@ import java.util.List;
 
 @Schema(description = "모델 메인 뷰 Response DTO")
 public record ModelMainResponse(
-        PageInfo pageInfo,
+        int page,
+        int size,
         ModelApplyStatus status,
         String userName,
         List<OfferResponse> offers
