f96089ca5a8e2dc3f736d74d84d4b0fea8315709
hellozo0 <ahju0011@gmail.com>
2024-01-08 16:20:01 +0900

    #43 [fix] Day ENUM key 값 오타 수정

--- src/main/java/com/moddy/server/domain/day_off/DayOfWeek.java
+++ src/main/java/com/moddy/server/domain/day_off/DayOfWeek.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum DayOfWeek {
-    NOTTING("없음"), MON("월"), TUE("화"), WED("수"), THU("목"), FRI("금"), SAT("토"), SUN("일");
+    NOTHING("없음"), MON("월"), TUE("화"), WED("수"), THU("목"), FRI("금"), SAT("토"), SUN("일");
 
     private final String value;
 }
