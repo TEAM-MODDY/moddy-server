ab8ad2a83935dd482b32f18475c7089f1d362670
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:45:28 +0900

    #35 [feat] ModelMainReposponse String에서 ENUM으로 적용

--- src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
+++ src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
@@ -1,6 +1,7 @@
 package com.moddy.server.controller.model.dto.response;
 
 import com.moddy.server.common.dto.PageInfo;
+import com.moddy.server.domain.model.ModelApplyStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
@@ -8,8 +9,9 @@ import java.util.List;
 @Schema(description = "모델 메인 뷰 Response DTO")
 public record ModelMainResponse(
         PageInfo pageInfo,
-        String status,
+        ModelApplyStatus status,
         String userName,
-        List<OfferResponse> offer
+        List<OfferResponse> offers
 ) {
+
 }
