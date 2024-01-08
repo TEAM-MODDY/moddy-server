1500a2776386ec4ff513e4f9c3df5befc99faac1
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:39:58 +0900

    #35 [feat] Model Jpa Repository 생성

--- src/main/java/com/moddy/server/domain/model/repository/ModelJpaRepository.java
+++ src/main/java/com/moddy/server/domain/model/repository/ModelJpaRepository.java
@@ -1,10 +1,15 @@
 package com.moddy.server.domain.model.repository;
 
 import com.moddy.server.domain.user.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ModelRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+@Repository
+public interface ModelJpaRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUser(Long userId);
 }
+
