b79ead3a738e18a1322770e11419dd988b0d0484
hellozo0 <ahju0011@gmail.com>
2024-01-08 16:07:43 +0900

    #43 [feat] 헤어관련 enum value 값 추가

--- src/main/java/com/moddy/server/domain/hair_model_application/HairLength.java
+++ src/main/java/com/moddy/server/domain/hair_model_application/HairLength.java
@@ -1,5 +1,12 @@
 package com.moddy.server.domain.hair_model_application;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum HairLength {
-    SHORT, ABOVE_SHOULDER, UNDER_SHOULDER, UNDER_WAIST
+    SHORT("숏"), ABOVE_SHOULDER("단발"), UNDER_SHOULDER("어깨아래"), UNDER_WAIST("허리아래");
+
+    private final String value;
 }
--- src/main/java/com/moddy/server/domain/prefer_hair_style/HairStyle.java
+++ src/main/java/com/moddy/server/domain/prefer_hair_style/HairStyle.java
@@ -1,5 +1,12 @@
 package com.moddy.server.domain.prefer_hair_style;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum HairStyle {
-    CUT, ALL_COLOR, ALL_DECOLOR, SETTING_PERM, NORMAL_PERM, STRAIGHTENING
+    CUT("컷"), ALL_COLOR("전체염색"), ALL_DECOLOR("전체탈색"), SETTING_PERM("세팅펌"), NORMAL_PERM("일반펌"), STRAIGHTENING("매직");
+
+    private final String value;
 }
