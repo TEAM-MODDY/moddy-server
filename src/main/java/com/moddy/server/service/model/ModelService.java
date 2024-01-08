0e9a053e5743b78dff3c09ca27ce14c29057039e
hellozo0 <ahju0011@gmail.com>
2024-01-09 00:41:11 +0900

    #35 [feat] OfferResponse 배열 반복문으로 조회 후에 return

--- src/main/java/com/moddy/server/service/model/ModelService.java
+++ src/main/java/com/moddy/server/service/model/ModelService.java
@@ -12,6 +12,7 @@ import com.moddy.server.domain.har_service_offer.HairServiceOffer;
 import com.moddy.server.domain.har_service_offer.repository.HairServiceOfferJpaRepository;
 import com.moddy.server.domain.model.ModelApplyStatus;
 import com.moddy.server.domain.model.repository.ModelJpaRepository;
+import com.moddy.server.domain.prefer_offer_condition.OfferCondition;
 import com.moddy.server.domain.prefer_offer_condition.PreferOfferCondition;
 import com.moddy.server.domain.prefer_offer_condition.repository.PreferOfferConditionJpaRepository;
 import com.moddy.server.domain.user.User;
@@ -23,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -66,15 +68,10 @@ public class ModelService {
         PageInfo pageInfo = new PageInfo(page, size);
 
 
-        //STATUS 계산해야함 -> 그럴려면 해당 유저의 userId를 알아야함, 그리고 그 유저 아이디로 Application 조회
         boolean applyStatus = hairModelApplicationJpaRepository.existsByUser(userId);
-
-        //STATUS 계산해야함 -> 그럴려면 해당 유저의 userId를 알아야함, 그리고 그 유저 아이디로 Offer 조회
         boolean offerStatus = hairServiceOfferJpaRepository.existsByUser(userId);
-
         ModelApplyStatus modelApplyStatus = calModelStatus(applyStatus, offerStatus);
         User user = modelJpaRepository.findByUser(userId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MODEL_INFO));
-
 
         if(modelApplyStatus != ModelApplyStatus.APPLY_AND_OFFER){
             return new ModelMainResponse(
@@ -85,45 +82,31 @@ public class ModelService {
             );
         }
 
-        //offers 조회
-        List<HairServiceOffer> hairServiceOffers = hairServiceOfferJpaRepository.findAllByUser(userId);
 
+        List<HairServiceOffer> hairServiceOffers = hairServiceOfferJpaRepository.findAllByUser(userId);
         List<OfferResponse> offerResponseList = new ArrayList<>();
-        //preferOfferConditionJpaRepository
 
         for (int i = 0; i < hairServiceOffers.size(); i++){
             Designer designer = hairServiceOffers.get(i).getDesigner();
-            PreferOfferCondition preferOfferCondition = preferOfferConditionJpaRepository.findByHairServiceOffer(hairServiceOffers.get(i).getId()).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_PREFER_OFFER_CONDITION));;
-            offerResponseList.add(
-                    i,
+            List<PreferOfferCondition> preferOfferCondition = preferOfferConditionJpaRepository.findTop2ByHairServiceOffer(hairServiceOffers.get(i).getId());
+            List<OfferCondition> offerConditionTop2List = preferOfferCondition.stream().map(PreferOfferCondition::getOfferCondition).collect(Collectors.toList());
+
+            OfferResponse offerResponse = new OfferResponse(
                     hairServiceOffers.get(i).getId(),
                     designer.getProfileImgUrl(),
                     designer.getName(),
                     designer.getHairShop().getName(),
+                    offerConditionTop2List,
+                    hairServiceOffers.get(i).getIsClicked());
 
-
-            );
-
+            offerResponseList.add(offerResponse);
         }
-
-        //offers
-//        "offerId" : 1
-//        "imgUrl" : "http://디자이너이미지겠죠?",
-//                "name" : "백모디1",
-//                "shopName" : "모디 헤어1",
-//                "conditions" : [
-//        "전액 무료",
-//                "소정의 재료비"
-//								],
-//        "isClicked" : true
-        //isClicked  로직은 상세보기 했을 떄 구현하는 걸로
-
 
         return new ModelMainResponse(
                 pageInfo,
                 modelApplyStatus,
                 user.getName(),
-
+                offerResponseList
         );
     }
 
