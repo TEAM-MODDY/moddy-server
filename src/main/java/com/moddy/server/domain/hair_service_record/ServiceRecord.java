d4114ef595b26eef1684623a91c8d81d80c8bf9f
pkl0912 <pklpkl0912@gmail.com>
2024-01-08 16:12:35 +0900

    #43 [refactor] 시술이력 enum 수정

--- src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecord.java
+++ src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecord.java
@@ -1,5 +1,12 @@
 package com.moddy.server.domain.hair_service_record;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ServiceRecord {
-    PERM, DECOLOR, BLACK, COLOR
+    PERM("펌"), DECOLOR("탈색"), BLACK("블랙 염색"), COLOR("컬러 염색");
+
+    private final String value;
 }
--- src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecordTerm.java
+++ src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecordTerm.java
@@ -1,6 +1,11 @@
 package com.moddy.server.domain.hair_service_record;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
 public enum ServiceRecordTerm {
+    UNDER_ONE("1 개월 미만"), ONE_THREE("1 - 3 개월"), FOUR_SIX("4 - 6 개월"), SEVEN_TWELVE("7 - 12 개월"), ABOVE_TWELVE("12 개월 초과");
 
-    UNDER_ONE, ONE_THREE, FOUR_SIX, SEVEN_TWELVE, ABOVE_TWELVE
+    private final String value;
 }
