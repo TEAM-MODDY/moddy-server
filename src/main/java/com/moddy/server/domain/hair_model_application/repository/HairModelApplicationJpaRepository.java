1b68e94ae57158a5b4136ca5eae2d291dc393ab6
hellozo0 <ahju0011@gmail.com>
2024-01-08 22:28:45 +0900

    #35 [feat] 모델의 지원서 작성 여부 조회 jpa

---
+++ src/main/java/com/moddy/server/domain/hair_model_application/repository/HairModelApplicationJpaRepository.java
@@ -0,0 +1,13 @@
+package com.moddy.server.domain.hair_model_application.repository;
+
+import com.moddy.server.domain.hair_model_application.HairModelApplication;
+import com.moddy.server.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HairModelApplicationJpaRepository extends JpaRepository<HairModelApplication, Long> {
+
+    Boolean existsByUser(Long userId);
+
+}
