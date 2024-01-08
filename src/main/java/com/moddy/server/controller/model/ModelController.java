ec4f68cfca6a0ce8627b8a4c3ef73f11168429ee
hellozo0 <ahju0011@gmail.com>
2024-01-09 03:27:48 +0900

    #35 [feat] ModelController Security 어노테이션 추가

--- src/main/java/com/moddy/server/controller/model/ModelController.java
+++ src/main/java/com/moddy/server/controller/model/ModelController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.ErrorResponse;
@@ -27,17 +28,14 @@ public class ModelController {
     private final ModelService modelService;
 
     @Operation(summary = "모델 메인 뷰 조회", description = "모델 메인 뷰 조회 API입니다.")
-    @Parameters({
-            @Parameter(name = "page", description = "페이지 넘버"),
-            @Parameter(name = "size", description = "페이징 해서 가져올 정보들의 크기"),
-    })
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "모델 메인뷰 조회 성공", content = @Content(schema = @Schema(implementation = ModelMainResponse.class))),
             @ApiResponse(responseCode = "401", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "404", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
     @GetMapping
-    public SuccessResponse<ModelMainResponse> getMain(@UserId Long userId, @RequestParam int page, @RequestParam int size){
+    @SecurityRequirement(name = "JWT Auth")
+    public SuccessResponse<ModelMainResponse> getMain(@Parameter(hidden = true) @UserId Long userId, @Parameter(name = "page", description = "페이지 ") @RequestParam(value = "page") int page, @Parameter(name = "size", description = "페이지 ") @RequestParam(value = "size") int size){
         return SuccessResponse.success(SuccessCode.FIND_MODEL_MAIN_INFO_SUCCESS, modelService.getModelMainInfo(userId, page, size));
     }
 
