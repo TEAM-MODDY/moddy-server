a78edef7d77fdee7207679ead15542444775f643
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:43:58 +0900

    #35 [feat]HairModelApplication Getter 추가

--- src/main/java/com/moddy/server/domain/hair_model_application/HairModelApplication.java
+++ src/main/java/com/moddy/server/domain/hair_model_application/HairModelApplication.java
@@ -5,8 +5,13 @@ import com.moddy.server.domain.designer.Designer;
 import com.moddy.server.domain.user.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 public class HairModelApplication extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
