b4f32910b8906690f5a48aee458ceaee61063527
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:38:50 +0900

    #35 [feat] User Getter 추가

--- src/main/java/com/moddy/server/domain/user/User.java
+++ src/main/java/com/moddy/server/domain/user/User.java
@@ -8,8 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 public class User extends BaseTimeEntity {
 
     @Id
