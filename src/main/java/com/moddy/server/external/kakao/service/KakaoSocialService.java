c7bd7e7383ac3f8a778e315fb9121cbca6f338d7
KWY <wodrk1001@gmail.com>
2024-01-08 23:43:31 +0900

    #51 [fix] kakao code module 수정

--- src/main/java/com/moddy/server/external/kakao/service/KakaoSocialService.java
+++ src/main/java/com/moddy/server/external/kakao/service/KakaoSocialService.java
@@ -1,15 +1,15 @@
 package com.moddy.server.external.kakao.service;
 
-
+import com.moddy.server.common.exception.model.BadRequestException;
 import com.moddy.server.external.kakao.dto.response.KakaoAccessTokenResponse;
 import com.moddy.server.external.kakao.dto.response.KakaoUserResponse;
 import com.moddy.server.external.kakao.feign.KakaoApiClient;
 import com.moddy.server.external.kakao.feign.KakaoAuthApiClient;
-import com.moddy.server.controller.auth.dto.request.SocialLoginRequest;
-import com.moddy.server.controller.auth.dto.response.SocialLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import static com.moddy.server.common.exception.enums.ErrorCode.INVALID_KAKAO_CODE_EXCEPTION;
 
 
 @Service
@@ -18,27 +18,23 @@ public class KakaoSocialService extends SocialService {
 
     @Value("${kakao.client-id}")
     private String clientId;
-
-
     private final KakaoAuthApiClient kakaoAuthApiClient;
     private final KakaoApiClient kakaoApiClient;
 
     @Override
-    public SocialLoginResponse login(SocialLoginRequest request) {
-
-
+    public String getIdFromKakao(String kakaoCode) {
+        if (kakaoCode == null) throw new BadRequestException(INVALID_KAKAO_CODE_EXCEPTION);
         // Authorization code로 Access Token 불러오기
         KakaoAccessTokenResponse tokenResponse = kakaoAuthApiClient.getOAuth2AccessToken(
                 "authorization_code",
                 clientId,
                 "http://localhost:3000/login/oauth2/code/kakao",
-                request.code()
+                kakaoCode
         );
 
         // Access Token으로 유저 정보 불러오기
         KakaoUserResponse userResponse = kakaoApiClient.getUserInformation("Bearer " + tokenResponse.accessToken());
 
-        return new SocialLoginResponse(String.valueOf(userResponse.id()));
-
+        return String.valueOf(userResponse.id());
     }
 }
--- src/main/java/com/moddy/server/external/kakao/service/SocialService.java
+++ src/main/java/com/moddy/server/external/kakao/service/SocialService.java
@@ -1,10 +1,7 @@
 package com.moddy.server.external.kakao.service;
 
 
-import com.moddy.server.controller.auth.dto.request.SocialLoginRequest;
-import com.moddy.server.controller.auth.dto.response.SocialLoginResponse;
-
 public abstract class SocialService {
-    public abstract SocialLoginResponse login(SocialLoginRequest request);
+    public abstract String getIdFromKakao(String kakaoCode);
 
 }
