141d08420b91273fa9bef9eccd03ed62fbb6d70f
KWY <wodrk1001@gmail.com>
2024-01-08 23:49:42 +0900

    #51 [chore] 로그이 api 스웨거 적용

--- src/main/java/com/moddy/server/controller/auth/AuthController.java
+++ src/main/java/com/moddy/server/controller/auth/AuthController.java
@@ -1,8 +1,14 @@
 package com.moddy.server.controller.auth;
 
+import com.moddy.server.common.dto.ErrorResponse;
 import com.moddy.server.common.dto.SuccessResponse;
 import com.moddy.server.controller.auth.dto.response.LoginResponseDto;
 import com.moddy.server.service.auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +25,13 @@ import static com.moddy.server.common.exception.enums.SuccessCode.SOCIAL_LOGIN_S
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "카카오 로그인 성공입니다."),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 카카오 코드를 입력했습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "해당 유저는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/login")
     public SuccessResponse<LoginResponseDto> login(@RequestHeader("kakaoCode") String kakaoCode) {
         return SuccessResponse.success(SOCIAL_LOGIN_SUCCESS, authService.login(kakaoCode));
