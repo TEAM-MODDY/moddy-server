6bef29d4f7f83d6fa34cc15ab1c40a2a9cac2d94
hellozo0 <ahju0011@gmail.com>
2024-01-08 18:25:41 +0900

    #35 [feat] Success Code ENUM 추가

--- src/main/java/com/moddy/server/common/exception/enums/SuccessCode.java
+++ src/main/java/com/moddy/server/common/exception/enums/SuccessCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessCode {
 
-
+    FIND_MODEL_MAIN_INFO_SUCCESS(HttpStatus.OK, "모델 메인 뷰 조회 성공"),
     SOCIAL_LOGIN_SUCCESS(HttpStatus.OK, "소셜 로그인 성공");
 
     private final HttpStatus httpStatus;
