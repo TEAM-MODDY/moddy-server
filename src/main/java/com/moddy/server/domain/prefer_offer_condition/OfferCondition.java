1293e3a1a7d78868d307cbcec8828fbb29c76640
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:16:12 +0900

    #35 [feat] 제안 조건 ENUM value 추가

--- src/main/java/com/moddy/server/domain/prefer_offer_condition/OfferCondition.java
+++ src/main/java/com/moddy/server/domain/prefer_offer_condition/OfferCondition.java
@@ -1,5 +1,12 @@
 package com.moddy.server.domain.prefer_offer_condition;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum OfferCondition {
-    CAMERA, MASK, SNS, PHOTOSHOP, FREE, SMALL_PAY
+    CAMERA("얼굴 촬영"), MASK("마스크 착용"), SNS("SNS 게시"), PHOTOSHOP("포토샵 보정"), FREE("전액 무료"), SMALL_PAY("소정의 약값");
+
+    private final String value;
 }
