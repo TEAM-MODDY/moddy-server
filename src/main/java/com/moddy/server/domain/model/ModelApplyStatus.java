0912b6c1a828877065297080dcebbf79f733f4ba
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:41:32 +0900

    #35 [feat] 지원상태 ENUM에 Getter 추가

--- src/main/java/com/moddy/server/domain/model/ModelApplyStatus.java
+++ src/main/java/com/moddy/server/domain/model/ModelApplyStatus.java
@@ -1,8 +1,10 @@
 package com.moddy.server.domain.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum ModelApplyStatus {
     NOTHING, APPLY, APPLY_AND_OFFER
 }
