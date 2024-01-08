09e9479a844a4d7d0e9612ea455f59c8b5806279
hellozo0 <ahju0011@gmail.com>
2024-01-08 22:29:56 +0900

    #35 [refactor] 페이징 관련 정보 PageInfo로 변경해서 Dto 반환

--- src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
+++ src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
@@ -1,13 +1,13 @@
 package com.moddy.server.controller.model.dto.response;
 
+import com.moddy.server.common.dto.PageInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 @Schema(description = "모델 메인 뷰 Response DTO")
 public record ModelMainResponse(
-        int page,
-        int size,
+        PageInfo pageInfo,
         String status,
         String userName,
         List<OfferResponse> offer
