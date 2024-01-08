7f8acc1b7d07bd5900c8eaa2e90809fef2f2ce2c
hellozo0 <ahju0011@gmail.com>
2024-01-08 23:43:18 +0900

    #35 [feat] PreferOfferCondition Repository 추가

---
+++ src/main/java/com/moddy/server/domain/prefer_offer_condition/repository/PreferOfferConditionJpaRepository.java
@@ -0,0 +1,13 @@
+package com.moddy.server.domain.prefer_offer_condition.repository;
+
+import com.moddy.server.domain.prefer_offer_condition.PreferOfferCondition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PreferOfferConditionJpaRepository extends JpaRepository<PreferOfferCondition, Long> {
+
+    Optional<PreferOfferCondition> findByHairServiceOffer(Long offerId);
+}
