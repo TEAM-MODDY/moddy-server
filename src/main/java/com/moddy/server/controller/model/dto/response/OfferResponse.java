d7b6f8e439db6c3a3b2dde1ce9ac2ed939ac2223
hellozo0 <ahju0011@gmail.com>
2024-01-09 00:37:04 +0900

    #35 [feat] OfferResponse type 변경

--- src/main/java/com/moddy/server/controller/model/dto/response/OfferResponse.java
+++ src/main/java/com/moddy/server/controller/model/dto/response/OfferResponse.java
@@ -1,13 +1,14 @@
 package com.moddy.server.controller.model.dto.response;
 
+import com.moddy.server.domain.prefer_offer_condition.OfferCondition;
 import java.util.List;
 
 public record OfferResponse(
-        int offerId,
+        Long offerId,
         String imgUrl,
         String name,
         String shopName,
-        List conditions,
+        List<OfferCondition> conditions,
         boolean isClicked
 ) {
 }
