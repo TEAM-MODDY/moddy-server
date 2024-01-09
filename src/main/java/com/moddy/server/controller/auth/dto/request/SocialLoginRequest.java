f5acee38c88e93c7446498aa65adfad09226201a
pkl0912 <pklpkl0912@gmail.com>
2024-01-09 20:39:05 +0900

    #47 [feat] request 객체 구현

--- src/main/java/com/moddy/server/controller/auth/dto/request/SocialLoginRequest.java
+++ src/main/java/com/moddy/server/controller/auth/dto/request/SocialLoginRequest.java
@@ -3,4 +3,7 @@ package com.moddy.server.controller.auth.dto.request;
 public record SocialLoginRequest(
         String code
 ) {
+    public static SocialLoginRequest of(String code) {
+        return new SocialLoginRequest(code);
+    }
 }
