a1822e73a1268a29d5f2b13377cf51e53ed798f5
KWY <wodrk1001@gmail.com>
2024-01-10 00:51:42 +0900

    #65 [feat] kakao 로그인 도메인 적용

--- src/main/java/com/moddy/server/external/kakao/service/KakaoSocialService.java
+++ src/main/java/com/moddy/server/external/kakao/service/KakaoSocialService.java
@@ -15,22 +15,23 @@ public class KakaoSocialService extends SocialService {
 
     @Value("${kakao.client-id}")
     private String clientId;
+    private static final String Bearer = "Bearer ";
+    private static final String GRANT_TYPE = "authorization_code";
+    private static final String KAKAO_ROUTER = "/login/oauth2/code/kakao";
     private final KakaoAuthApiClient kakaoAuthApiClient;
     private final KakaoApiClient kakaoApiClient;
 
     @Override
-    public String getIdFromKakao(String kakaoCode) {
-        // Authorization code로 Access Token 불러오기
+    public String getIdFromKakao(String baseUrl, String kakaoCode) {
+        String redirectUrl = baseUrl + KAKAO_ROUTER;
         KakaoAccessTokenResponse tokenResponse = kakaoAuthApiClient.getOAuth2AccessToken(
-                "authorization_code",
+                GRANT_TYPE,
                 clientId,
-                "http://localhost:3000/login/oauth2/code/kakao",
+                redirectUrl,
                 kakaoCode
         );
 
-        // Access Token으로 유저 정보 불러오기
-        KakaoUserResponse userResponse = kakaoApiClient.getUserInformation("Bearer " + tokenResponse.accessToken());
-
+        KakaoUserResponse userResponse = kakaoApiClient.getUserInformation(Bearer + tokenResponse.accessToken());
         return String.valueOf(userResponse.id());
     }
 }
