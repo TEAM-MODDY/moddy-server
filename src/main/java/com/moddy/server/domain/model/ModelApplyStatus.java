b5737cbf19ceb88946d5b04d5d35013aa0d325af
hellozo0 <ahju0011@gmail.com>
2024-01-09 16:24:42 +0900

    #35 [fix] 안쓰는 어노테이션 삭제

--- src/main/java/com/moddy/server/domain/model/ModelApplyStatus.java
+++ src/main/java/com/moddy/server/domain/model/ModelApplyStatus.java
@@ -1,9 +1,7 @@
 package com.moddy.server.domain.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public enum ModelApplyStatus {
     NOTHING, APPLY, APPLY_AND_OFFER
