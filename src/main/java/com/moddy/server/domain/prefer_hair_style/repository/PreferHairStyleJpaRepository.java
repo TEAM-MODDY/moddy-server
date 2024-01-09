f1e5d308a6373223af38f9fbd002fcbf8afbabff
hellozo0 <ahju0011@gmail.com>
2024-01-09 21:43:34 +0900

    #36 [feat] 지원서 Id로 선호 스타일 조회

---
+++ src/main/java/com/moddy/server/domain/prefer_hair_style/repository/PreferHairStyleJpaRepository.java
@@ -0,0 +1,13 @@
+package com.moddy.server.domain.prefer_hair_style.repository;
+
+import com.moddy.server.domain.prefer_hair_style.PreferHairStyle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PreferHairStyleJpaRepository extends JpaRepository<PreferHairStyle, Long> {
+
+    List<PreferHairStyle> findAllById(Long applicationId);
+}
