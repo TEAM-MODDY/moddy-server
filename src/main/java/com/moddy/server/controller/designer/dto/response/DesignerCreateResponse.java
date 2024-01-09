de2ada08e0d8382fe81cf81512fa680e6f9e9c95
pkl0912 <pklpkl0912@gmail.com>
2024-01-09 20:39:41 +0900

    #47 [feat] response 객체 작성

--- src/main/java/com/moddy/server/controller/designer/dto/response/DesignerCreateResponse.java
+++ src/main/java/com/moddy/server/controller/designer/dto/response/DesignerCreateResponse.java
@@ -1,8 +1,8 @@
 package com.moddy.server.controller.designer.dto.response;
 
-import com.moddy.server.config.jwt.JwtService;
-import com.moddy.server.domain.designer.Designer;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "디자이너 회원가입 뷰 Response DTO")
 public record DesignerCreateResponse(
         String accessToken,
         String refreshToken
