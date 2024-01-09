eb07a42fefee14d3f9d79e96dd1255ac0328e718
hellozo0 <ahju0011@gmail.com>
2024-01-09 20:03:45 +0900

    #36 [feat] Portfolio Getter 추가

--- src/main/java/com/moddy/server/domain/designer/Portfolio.java
+++ src/main/java/com/moddy/server/domain/designer/Portfolio.java
@@ -2,8 +2,10 @@ package com.moddy.server.domain.designer;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Embeddable
+@Getter
 public class Portfolio {
     @NotNull
     private String instagramUrl;
