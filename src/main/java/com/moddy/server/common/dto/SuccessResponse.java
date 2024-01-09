be407f9409e3dd21a2a78d2c908cbe311a3203a5
hellozo0 <ahju0011@gmail.com>
2024-01-10 04:51:32 +0900

    #37 [fix] 안쓰는 import 삭제

--- src/main/java/com/moddy/server/common/dto/SuccessResponse.java
+++ src/main/java/com/moddy/server/common/dto/SuccessResponse.java
@@ -4,14 +4,12 @@ import com.moddy.server.common.exception.enums.SuccessCode;
 import lombok.*;
 
 @Getter
-@ToString
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SuccessResponse<T> {
     private final int code;
     private final String message;
     private T data;
-
 
     public static SuccessResponse success(SuccessCode successCode) {
         return new SuccessResponse<>(successCode.getHttpStatus().value(), successCode.getMessage());
