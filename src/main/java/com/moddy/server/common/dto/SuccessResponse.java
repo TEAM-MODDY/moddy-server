c848de93b396b92ad1b40ec521077b922718e47c
hellozo0 <ahju0011@gmail.com>
2024-01-10 02:22:02 +0900

    #37 [feat] SuccessCode Data 없는 경우 함수 추가

--- src/main/java/com/moddy/server/common/dto/SuccessResponse.java
+++ src/main/java/com/moddy/server/common/dto/SuccessResponse.java
@@ -1,18 +1,21 @@
 package com.moddy.server.common.dto;
 
 import com.moddy.server.common.exception.enums.SuccessCode;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @ToString
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SuccessResponse<T> {
     private final int code;
     private final String message;
-    private final T data;
+    private T data;
+
+
+    public static SuccessResponse success(SuccessCode successCode) {
+        return new SuccessResponse<>(successCode.getHttpStatus().value(), successCode.getMessage());
+    }
 
     public static <T> SuccessResponse<T> success(SuccessCode successCode, T data) {
         return new SuccessResponse<>(successCode.getHttpStatus().value(), successCode.getMessage(), data);
