de834048ed17bdf9400d3c6a33072f1ba700cff9
pkl0912 <pklpkl0912@gmail.com>
2024-01-09 20:42:15 +0900

    #47 [refactor] basetimeEntity 수정

--- src/main/java/com/moddy/server/domain/BaseTimeEntity.java
+++ src/main/java/com/moddy/server/domain/BaseTimeEntity.java
@@ -2,13 +2,19 @@ package com.moddy.server.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@SuperBuilder
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
