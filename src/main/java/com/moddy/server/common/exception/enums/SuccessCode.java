8a798c215f5c32dba289921e657dcebc9907c001
hellozo0 <ahju0011@gmail.com>
2024-01-09 17:39:30 +0900

    #36 [feat] 제안서 상세보기 성공 SuccessCode 추가

--- src/main/java/com/moddy/server/common/exception/enums/SuccessCode.java
+++ src/main/java/com/moddy/server/common/exception/enums/SuccessCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     FIND_MODEL_MAIN_INFO_SUCCESS(HttpStatus.OK, "모델 메인 뷰 조회 성공"),
+    FIND_MODEL_DETAIL_OFFER_SUCCESS(HttpStatus.OK, "제안서 조회 성공"),
     SOCIAL_LOGIN_SUCCESS(HttpStatus.OK, "카카오 로그인 성공입니다.");
 
     private final HttpStatus httpStatus;
