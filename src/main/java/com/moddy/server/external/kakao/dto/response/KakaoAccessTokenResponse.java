0a2e35b3aca29179a5a365824d5e4ed15b702931
KWY <wodrk1001@gmail.com>
2024-01-09 15:52:07 +0900

    #51 [feat] json naming 어노테이션 추가

--- src/main/java/com/moddy/server/external/kakao/dto/response/KakaoAccessTokenResponse.java
+++ src/main/java/com/moddy/server/external/kakao/dto/response/KakaoAccessTokenResponse.java
@@ -1,7 +1,8 @@
 package com.moddy.server.external.kakao.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoAccessTokenResponse(String accessToken, String refreshToken) {
-    public static KakaoAccessTokenResponse of(String accessToken, String refreshToken) {
-        return new KakaoAccessTokenResponse(accessToken, refreshToken);
-    }
 }
