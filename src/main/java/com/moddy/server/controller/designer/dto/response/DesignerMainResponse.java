4287b62c64b4a56ffd03e3367585b4ed05a2c735
pkl0912 <pklpkl0912@gmail.com>
2024-01-10 16:31:48 +0900

    #71 [feat] response 추가

--- src/main/java/com/moddy/server/controller/designer/dto/response/DesignerMainResponse.java
+++ src/main/java/com/moddy/server/controller/designer/dto/response/DesignerMainResponse.java
@@ -2,8 +2,15 @@ package com.moddy.server.controller.designer.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.List;
+
 @Schema(description = "디자이너 메인 뷰 Response DTO")
 public record DesignerMainResponse(
+        int page,
+        int size,
+        String name,
+
+        List<HairModelApplicationResponse> applications
 
 ) {
 }
--- src/main/java/com/moddy/server/service/designer/DesignerService.java
+++ src/main/java/com/moddy/server/service/designer/DesignerService.java
@@ -1,24 +1,36 @@
 package com.moddy.server.service.designer;
 
 import com.moddy.server.common.dto.TokenPair;
+import com.moddy.server.common.exception.enums.ErrorCode;
+import com.moddy.server.common.exception.model.NotFoundException;
 import com.moddy.server.config.jwt.JwtService;
 import com.moddy.server.controller.designer.dto.request.DesignerCreateRequest;
 import com.moddy.server.controller.designer.dto.response.DesignerCreateResponse;
+import com.moddy.server.controller.designer.dto.response.DesignerMainResponse;
 import com.moddy.server.domain.day_off.DayOff;
 import com.moddy.server.domain.day_off.repository.DayOffJpaRepository;
 import com.moddy.server.domain.designer.Designer;
 import com.moddy.server.domain.designer.HairShop;
 import com.moddy.server.domain.designer.Portfolio;
 import com.moddy.server.domain.designer.repository.DesignerJpaRepository;
+import com.moddy.server.domain.hair_model_application.HairModelApplication;
+import com.moddy.server.domain.hair_model_application.repository.HairModelApplicationJpaRepository;
+import com.moddy.server.domain.hair_service_offer.HairServiceOffer;
 import com.moddy.server.domain.user.Role;
+import com.moddy.server.domain.user.User;
 import com.moddy.server.external.kakao.feign.KakaoApiClient;
 import com.moddy.server.external.kakao.feign.KakaoAuthApiClient;
 import com.moddy.server.external.kakao.service.KakaoSocialService;
 import com.moddy.server.external.s3.S3Service;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +44,12 @@ public class DesignerService {
     private final KakaoAuthApiClient kakaoAuthApiClient;
     private final KakaoApiClient kakaoApiClient;
     private final JwtService jwtService;
-
+    private final HairModelApplicationJpaRepository hairModelApplicationJpaRepository;
+    private Page<HairModelApplication> findApplications(Long userId, int page, int size){
+        PageRequest pageRequest = PageRequest.of(page-1, size, Sort.by(Sort.Direction.DESC,"id"));
+        Page<HairModelApplication> applicationPage = hairModelApplicationJpaRepository.findByUserId(userId, pageRequest);
+        return applicationPage;
+    }
     @Transactional
     public DesignerCreateResponse createDesigner(String baseUrl,String code, DesignerCreateRequest request) {
 
@@ -83,5 +100,11 @@ public class DesignerService {
     }
 
     @Transactional
-    public
+    public DesignerMainResponse getMainView(Long userId, int page, int size){
+        List<HairModelApplication> hairModelApplications= hairModelApplicationJpaRepository.findAllByUserId(userId);
+        User user = designerJpaRepository.findById(userId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MODEL_INFO));
+
+
+
+    }
 }
