69d641aa60b749f612fe64b48e37317658d0c682
hellozo0 <ahju0011@gmail.com>
2024-01-11 02:42:12 +0900

    #78 [fix] Gender Enum 타입으로 RequstBody 변경

--- src/main/java/com/moddy/server/controller/model/dto/request/ModelCreateRequest.java
+++ src/main/java/com/moddy/server/controller/model/dto/request/ModelCreateRequest.java
@@ -1,11 +1,12 @@
 package com.moddy.server.controller.model.dto.request;
 
+import com.moddy.server.domain.user.Gender;
 import java.util.List;
 
 public record ModelCreateRequest(
         String name,
         String year,
-        String gender,
+        Gender gender,
         String phoneNumber,
         Boolean isMarketingAgree,
         List<Long> preferRegions
