73ed93f78668c258f6695d84f9ee3ef9bb04665b
hellozo0 <ahju0011@gmail.com>
2024-01-11 00:23:30 +0900

    #78 [feat] Model 필요한 어노테이션 추가

--- src/main/java/com/moddy/server/domain/model/Model.java
+++ src/main/java/com/moddy/server/domain/model/Model.java
@@ -2,8 +2,16 @@ package com.moddy.server.domain.model;
 
 import com.moddy.server.domain.user.User;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class Model extends User {
     private String year;
 }
