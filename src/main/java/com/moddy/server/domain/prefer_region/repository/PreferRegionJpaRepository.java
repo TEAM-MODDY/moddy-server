a51fe231d24c43ace7be859ef01403a0440ed63f
hellozo0 <ahju0011@gmail.com>
2024-01-11 00:23:47 +0900

    #78 [feat] ReferRegion 레포지토리 추가

---
+++ src/main/java/com/moddy/server/domain/prefer_region/repository/PreferRegionJpaRepository.java
@@ -0,0 +1,8 @@
+package com.moddy.server.domain.prefer_region.repository;
+
+import com.moddy.server.domain.prefer_region.PreferRegion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PreferRegionJpaRepository extends JpaRepository<PreferRegion, Long> {
+
+}
