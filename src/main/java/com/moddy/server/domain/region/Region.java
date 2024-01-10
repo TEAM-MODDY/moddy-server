003e75be89185125edf02b35db49c429b4d2248a
hellozo0 <ahju0011@gmail.com>
2024-01-10 16:36:58 +0900

    #52 [feat] Region 도메인에 Getter 어노테이션 추가

--- src/main/java/com/moddy/server/domain/region/Region.java
+++ src/main/java/com/moddy/server/domain/region/Region.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Region extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
