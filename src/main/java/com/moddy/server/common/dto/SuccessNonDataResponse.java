b27ecff62febff0364b6e904b83a592ff73fcc3e
KWY <wodrk1001@gmail.com>
2024-01-12 01:34:48 +0900

    #98 [fix] 제네릭 제거

--- src/main/java/com/moddy/server/common/dto/SuccessNonDataResponse.java
+++ src/main/java/com/moddy/server/common/dto/SuccessNonDataResponse.java
@@ -6,11 +6,11 @@ import lombok.*;
 @Getter
 @ToString
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class SuccessNonDataResponse<T> {
+public class SuccessNonDataResponse {
     private final int code;
     private final String message;
 
     public static SuccessNonDataResponse success(SuccessCode successCode) {
-        return new SuccessNonDataResponse<>(successCode.getHttpStatus().value(), successCode.getMessage());
+        return new SuccessNonDataResponse(successCode.getHttpStatus().value(), successCode.getMessage());
     }
 }
