b99c26177b329ccb1c45ca6b1980daeb8b1ce1a1
KWY <wodrk1001@gmail.com>
2024-01-14 23:16:58 +0900

    #122 [feat] hair length get value 추가

--- src/main/java/com/moddy/server/service/designer/DesignerService.java
+++ src/main/java/com/moddy/server/service/designer/DesignerService.java
@@ -224,7 +224,7 @@ public class DesignerService {
         ApplicationInfoResponse applicationInfoResponse = new ApplicationInfoResponse(
                 applicationId,
                 hairModelApplication.getModelImgUrl(),
-                hairModelApplication.getHairLength(),
+                hairModelApplication.getHairLength().getValue(),
                 preferhairStyleList,
                 recordResponseList,
                 hairModelApplication.getHairDetail(),
