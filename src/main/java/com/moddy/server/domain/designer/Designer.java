bf90226ddba4f5a363d201c2b1989eca41544492
pkl0912 <pklpkl0912@gmail.com>
2024-01-16 00:16:23 +0900

    #118 [refactor]NotNull 살리기

--- src/main/java/com/moddy/server/domain/designer/Designer.java
+++ src/main/java/com/moddy/server/domain/designer/Designer.java
@@ -2,6 +2,7 @@ package com.moddy.server.domain.designer;
 
 import com.moddy.server.domain.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +23,9 @@ public class Designer extends User {
     @Embedded
     private Portfolio portfolio;
 
+    @NotNull
     private String introduction;
 
+    @NotNull
     private String kakaoOpenChatUrl;
 }
