e841fb45a64fcd7649bd6c03d1dcf6a137c1232f
hellozo0 <ahju0011@gmail.com>
2024-01-15 15:32:44 +0900

    #127 [fix] userName -> name 으로 변경

--- src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
+++ src/main/java/com/moddy/server/controller/model/dto/response/ModelMainResponse.java
@@ -11,7 +11,7 @@ public record ModelMainResponse(
         int size,
         int total,
         ModelApplyStatus status,
-        String userName,
+        String name,
         List<OfferResponse> offers
 ) {
 
