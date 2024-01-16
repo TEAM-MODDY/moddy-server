2d66f5758e719dee2418cfd482849d034bfe6c29
KWY <wodrk1001@gmail.com>
2024-01-16 23:42:16 +0900

    #133 [feat] gabia sms service 구현

--- src/main/java/com/moddy/server/external/sms/impl/GabiaSmsServiceImpl.java
+++ src/main/java/com/moddy/server/external/sms/impl/GabiaSmsServiceImpl.java
@@ -2,11 +2,13 @@ package com.moddy.server.external.sms.impl;
 
 import com.google.gson.Gson;
 import com.moddy.server.external.sms.SmsService;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,42 +17,96 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Objects;
 
+@Slf4j
 @Component
 public class GabiaSmsServiceImpl implements SmsService {
+    public static final String SMS_OAUTH_TOKEN_URL = "https://sms.gabia.com/oauth/token";
     public static final String SMS_SEND_URL = "https://sms.gabia.com/api/send/sms";
-    private static final String SMS_ID = "YOUR_SMS_ID"; // SMS ID 를 입력해 주세요.
-    private static final String ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"; // ACCESS TOKEN 을 입력해 주세요.
-    private static final String AUTH_VALUE = Base64.getEncoder()
-            .encodeToString(String.format("%s:%s", SMS_ID, ACCESS_TOKEN)
-                    .getBytes(StandardCharsets.UTF_8));
-    private static final String AUTHORIZATION_CODE = "Basic " + AUTH_VALUE;
+
+    @Value("${gabia.sms-id}")
+    private String smsId;
+    @Value("${gabia.api-key}")
+    private String apiKey;
+    @Value("${gabia.caller-phone-number}")
+    private String from;
+    @Value("${gabia.ref-key}")
+    private String refKey;
 
     @Override
     public boolean sendSms(final String to, final String verificationCode) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        String message = String.format(MESSAGE_FORM, verificationCode);
+        final String accessToken = getGabiaAccessToken();
+        final String smsValue = createAuthValue(accessToken);
 
-        RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                .addFormDataPart("phone", "RECEIPT_PHONE_NUMBER")
-                .addFormDataPart("callback", to)
-                .addFormDataPart("message", message)
-                .addFormDataPart("refkey", "YOUR_REF_KEY")
-                .build();
+        final String message = String.format(MESSAGE_FORM, verificationCode);
+        final RequestBody requestBody = createMessageRequestBody(to, message);
+        final Request request = createRequest(requestBody, smsValue, SMS_SEND_URL);
+        final Response response = callExecute(request);
 
-        Request request = new Request.Builder()
-                .url(SMS_SEND_URL)
+        final HashMap<String, String> result = getExecuteResult(response);
+
+        final boolean status = result.get("code").equals("200");
+        saveLog(status, result);
+
+        return status;
+    }
+
+    private void saveLog(final boolean status, final HashMap<String, String> result) {
+        final StringBuilder sb = new StringBuilder();
+        for (String key : result.keySet()) {
+            sb.append(String.format("%s: %s \n", key, result.get(key)));
+        }
+        if (status) log.info(sb.toString());
+        else log.error(sb.toString());
+    }
+
+    private String createAuthValue(final String key) {
+        return Base64.getEncoder()
+                .encodeToString(String.format("%s:%s", smsId, key)
+                        .getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String getGabiaAccessToken() throws IOException {
+        final String authValue = createAuthValue(apiKey);
+
+        final RequestBody requestBody = createAuthRequestBody();
+        final Request request = createRequest(requestBody, authValue, SMS_OAUTH_TOKEN_URL);
+        final Response response = callExecute(request);
+
+        final HashMap<String, String> result = getExecuteResult(response);
+        return result.get("access_token");
+    }
+
+    private HashMap<String, String> getExecuteResult(final Response response) throws IOException {
+        return new Gson().fromJson(Objects.requireNonNull(response.body()).string(), HashMap.class);
+    }
+
+    private Response callExecute(final Request request) throws IOException {
+        final OkHttpClient client = new OkHttpClient();
+        return client.newCall(request).execute();
+    }
+
+    private Request createRequest(final RequestBody requestBody, final String authorization, final String url) {
+        return new Request.Builder()
+                .url(url)
                 .post(requestBody)
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                .addHeader("Authorization", AUTHORIZATION_CODE)
+                .addHeader("Authorization", "Basic " + authorization)
                 .addHeader("cache-control", "no-cache")
                 .build();
+    }
 
-        Response response = client.newCall(request).execute();
+    private RequestBody createAuthRequestBody() {
+        return new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("grant_type", "client_credentials")
+                .build();
+    }
 
-        HashMap<String, String> result = new Gson().fromJson(Objects.requireNonNull(response.body()).string(), HashMap.class);
-        for (String key : result.keySet()) {
-            System.out.printf("%s: %s%n", key, result.get(key));
-        }
-        return true;
+    private RequestBody createMessageRequestBody(final String to, final String message) {
+        return new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("phone", to)
+                .addFormDataPart("callback", from)
+                .addFormDataPart("message", message)
+                .addFormDataPart("refkey", refKey)
+                .build();
     }
 }
