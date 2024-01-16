5b7a40eaee38dc1a1e3b391c0937aae46b348f2b
hellozo0 <ahju0011@gmail.com>
2024-01-17 03:08:46 +0900

    #155 [fix] ENUM에 Valeu -> name 바꾸는 메소드 변경

--- src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecordTerm.java
+++ src/main/java/com/moddy/server/domain/hair_service_record/ServiceRecordTerm.java
@@ -1,12 +1,7 @@
 package com.moddy.server.domain.hair_service_record;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.moddy.server.common.exception.enums.ErrorCode;
-import com.moddy.server.common.exception.model.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.stream.Stream;
 
 @Getter
 @AllArgsConstructor
@@ -14,12 +9,4 @@ public enum ServiceRecordTerm {
     UNDER_ONE("1 개월 미만"), ONE_THREE("1 - 3 개월"), FOUR_SIX("4 - 6 개월"), SEVEN_TWELVE("7 - 12 개월"), ABOVE_TWELVE("12 개월 초과");
 
     private final String value;
-
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public static ServiceRecordTerm findByServiceRecord(String serviceRecordTerm) {
-        return Stream.of(ServiceRecordTerm.values())
-                .filter(c -> c.name().equals(serviceRecordTerm))
-                .findFirst()
-                .orElseThrow(() -> new BadRequestException(ErrorCode.INVALID_HAIR_SERVICE_RECORD_TERM_EXCEPTION));
-    }
 }
