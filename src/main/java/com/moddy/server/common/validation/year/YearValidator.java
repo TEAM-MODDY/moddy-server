dbdb1d39087cefa3af8e5eab9867ea61de7c065f
hellozo0 <ahju0011@gmail.com>
2024-01-16 22:59:52 +0900

    #143 [feat] 1900~오늘날의 년도로 Validation

--- src/main/java/com/moddy/server/common/validation/year/YearValidator.java
+++ src/main/java/com/moddy/server/common/validation/year/YearValidator.java
@@ -5,6 +5,7 @@ import jakarta.validation.ConstraintValidatorContext;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class YearValidator implements ConstraintValidator<ValidYear, String> {
@@ -16,13 +17,12 @@ public class YearValidator implements ConstraintValidator<ValidYear, String> {
     @Override
     public boolean isValid(String year, ConstraintValidatorContext context) {
         try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
-            Date parsedDate = dateFormat.parse(year);
+            int parsedYear = Integer.parseInt(year);
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
-            // 현재 날짜와 비교하여 과거 여부를 확인
-            return parsedDate != null && parsedDate.before(new Date());
-        } catch (ParseException e) {
-            // 날짜 변환 실패 시 예외 처리
+            return parsedYear >= 1900 && parsedYear <= currentYear;
+
+        } catch (NumberFormatException  e) {
             return false;
         }
     }
