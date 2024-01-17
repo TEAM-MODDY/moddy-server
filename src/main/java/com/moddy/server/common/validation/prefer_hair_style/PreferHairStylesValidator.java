544973b4d625be58bc5f8e8f46cdd79e820a21f0
hellozo0 <ahju0011@gmail.com>
2024-01-17 15:15:00 +0900

    #149 [feat] 중복 불가 Validation List<PreferHair>

--- src/main/java/com/moddy/server/common/validation/prefer_hair_style/PreferHairStylesValidator.java
+++ src/main/java/com/moddy/server/common/validation/prefer_hair_style/PreferHairStylesValidator.java
@@ -1,21 +1,25 @@
 package com.moddy.server.common.validation.prefer_hair_style;
 
+import com.moddy.server.domain.prefer_hair_style.PreferHairStyle;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class PreferHairStylesValidator implements ConstraintValidator<ValidPreferHairStyles, List<String>> {
+public class PreferHairStylesValidator implements ConstraintValidator<ValidPreferHairStyles, List<PreferHairStyle>> {
 
     @Override
     public void initialize(ValidPreferHairStyles constraintAnnotation) {
     }
 
     @Override
-    public boolean isValid(List<String> preferHairStyles, ConstraintValidatorContext context) {
+    public boolean isValid(List<PreferHairStyle> preferHairStyles, ConstraintValidatorContext context) {
 
+        Set<PreferHairStyle> uniqueDays = new HashSet<>(preferHairStyles);
+        if(uniqueDays.size() != preferHairStyles.size()) return false;
         if (preferHairStyles.isEmpty() || preferHairStyles == null) return false;
-        if (preferHairStyles.size() > 6) return false;
 
         return true;
     }
