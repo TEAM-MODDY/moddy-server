ada8e098cc26ff33b5e4936c6815d4488b59cc97
hellozo0 <ahju0011@gmail.com>
2024-01-18 20:08:57 +0900

    #173 [fix]  URL 어떤 문자든지 (공백 포함) 0번 이상

--- src/main/java/com/moddy/server/controller/designer/dto/PortfolioDto.java
+++ src/main/java/com/moddy/server/controller/designer/dto/PortfolioDto.java
@@ -1,7 +1,7 @@
 package com.moddy.server.controller.designer.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -9,12 +9,12 @@ import lombok.*;
 @Builder
 public record PortfolioDto(
         @Schema(example = "http://instagram")
-        @NotBlank
-        @Pattern(regexp = "^[^\\s]+$", message = "url에는 공백(whitespaces)이 들어갈 수 없습니다.")
+        @NotNull
+        @Pattern(regexp = "^[\\s\\S]*$", message = "url에는 공백(whitespaces)이 들어갈 수 없습니다.")
         String instagramUrl,
         @Schema(example = "http://naver")
-        @NotBlank
-        @Pattern(regexp = "^[^\\s]+$", message = "url에는 공백(whitespaces)이 들어갈 수 없습니다.")
+        @NotNull
+        @Pattern(regexp = "^[\\s\\S]*$", message = "url에는 공백(whitespaces)이 들어갈 수 없습니다.")
         String naverPlaceUrl
 ) {
 }
