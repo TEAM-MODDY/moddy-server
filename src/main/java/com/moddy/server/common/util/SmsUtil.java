f77246a0bd14b91903f10ed32c2d9e1bf14daeba
KWY <wodrk1001@gmail.com>
2024-01-18 21:54:02 +0900

    #165 [feat] sms 기능 수정

--- src/main/java/com/moddy/server/common/util/SmsUtil.java
+++ src/main/java/com/moddy/server/common/util/SmsUtil.java
@@ -9,9 +9,17 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class SmsUtil {
+    private static final String VERIFICATION_MESSAGE_FORM = "[%s] moddy에서 보낸 인증번호입니다.";
+    private static final String OFFER_MESSAGE_FORM = "[moddy] %s님, 새로운 헤어모델 제안이 도착했어요!\nmoddy.kr";
     private final SmsService smsService;
 
-    public boolean sendVerificationCode(String to, String verificationCode) throws IOException {
-        return smsService.sendSms(to, verificationCode);
+    public boolean sendVerificationCode(final String to, final String verificationCode) throws IOException {
+        final String verificationCodeMessage = String.format(VERIFICATION_MESSAGE_FORM, verificationCode);
+        return smsService.sendSms(to, verificationCodeMessage);
+    }
+
+    public boolean sendOfferToModel(final String to, final String modelName) throws IOException {
+        final String modelMessage = String.format(OFFER_MESSAGE_FORM, modelName);
+        return smsService.sendSms(to, modelMessage);
     }
 }
