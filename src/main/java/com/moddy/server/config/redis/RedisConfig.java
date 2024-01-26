d61cc1fb78d66b36a865a9dabc51e3ae59a092c7
KWY <wodrk1001@gmail.com>
2024-01-27 00:46:23 +0900

    #194 [feat] redis template primary 등록

--- src/main/java/com/moddy/server/config/redis/RedisConfig.java
+++ src/main/java/com/moddy/server/config/redis/RedisConfig.java
@@ -3,6 +3,7 @@ package com.moddy.server.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -22,6 +23,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> getRedisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
