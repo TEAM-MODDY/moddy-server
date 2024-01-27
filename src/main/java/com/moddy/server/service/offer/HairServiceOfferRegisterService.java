01f4b2fb54a5a3841cd51a02766fccb2e05ef7d1
KWY <wodrk1001@gmail.com>
2024-01-27 17:03:31 +0900

    #202 [chore] 지원서 삭제 로직 이전

---
+++ src/main/java/com/moddy/server/service/offer/HairServiceOfferRegisterService.java
@@ -0,0 +1,24 @@
+package com.moddy.server.service.offer;
+
+import com.moddy.server.domain.hair_service_offer.HairServiceOffer;
+import com.moddy.server.domain.hair_service_offer.repository.HairServiceOfferJpaRepository;
+import com.moddy.server.domain.prefer_offer_condition.repository.PreferOfferConditionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class HairServiceOfferRegisterService {
+    private final PreferOfferConditionJpaRepository preferOfferConditionJpaRepository;
+    private final HairServiceOfferJpaRepository hairServiceOfferJpaRepository;
+
+    public void deleteModelHairServiceOfferInfos(final Long modelId) {
+        final List<HairServiceOffer> hairServiceOffers = hairServiceOfferJpaRepository.findAllByModelId(modelId);
+        hairServiceOffers.forEach(hairServiceOffer -> {
+            preferOfferConditionJpaRepository.deleteAllByHairServiceOffer(hairServiceOffer);
+            hairServiceOfferJpaRepository.deleteById(hairServiceOffer.getId());
+        });
+    }
+}
