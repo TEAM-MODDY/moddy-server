e399c127a25141e23325e7ce731ec41037376d71
KWY <wodrk1001@gmail.com>
2024-01-27 16:20:50 +0900

    #198 [feat] 조회 컨트롤러 서비스 생성

---
+++ src/main/java/com/moddy/server/controller/user/UserRetrieveController.java
@@ -0,0 +1,11 @@
+package com.moddy.server.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserRetrieveController {
+}
---
+++ src/main/java/com/moddy/server/service/user/UserRetrieveService.java
@@ -0,0 +1,9 @@
+package com.moddy.server.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserRetrieveService {
+}
