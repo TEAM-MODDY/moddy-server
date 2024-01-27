8435f4d918dc79dc17e0e00fa590103e11434249
KWY <wodrk1001@gmail.com>
2024-01-27 17:03:19 +0900

    #202 [chore] 모델 삭제 로직 이전

---
+++ src/main/java/com/moddy/server/service/model/ModelRegisterService.java
@@ -0,0 +1,22 @@
+package com.moddy.server.service.model;
+
+import com.moddy.server.domain.model.repository.ModelJpaRepository;
+import com.moddy.server.domain.prefer_region.repository.PreferRegionJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ModelRegisterService {
+    private final PreferRegionJpaRepository preferRegionJpaRepository;
+    private final ModelJpaRepository modelJpaRepository;
+
+    public void deleteModelInfo(final Long modelId) {
+        modelJpaRepository.deleteById(modelId);
+        deleteModelPreferRegions(modelId);
+    }
+
+    private void deleteModelPreferRegions(final Long modelId) {
+        preferRegionJpaRepository.deleteAllByModelId(modelId);
+    }
+}
