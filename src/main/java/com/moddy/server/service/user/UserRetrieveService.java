1a9c562538421bc06efbeb09b425bed1035be0f6
KWY <wodrk1001@gmail.com>
2024-01-27 16:24:23 +0900

    #200 [chore] 유저 조회 컨트롤러 서비스로 로직 이전

--- src/main/java/com/moddy/server/service/user/UserRetrieveService.java
+++ src/main/java/com/moddy/server/service/user/UserRetrieveService.java
@@ -1,9 +1,21 @@
 package com.moddy.server.service.user;
 
+import com.moddy.server.common.exception.model.NotFoundException;
+import com.moddy.server.controller.user.dto.response.UserDetailResponseDto;
+import com.moddy.server.domain.user.User;
+import com.moddy.server.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.moddy.server.common.exception.enums.ErrorCode.USER_NOT_FOUND_EXCEPTION;
 
 @Service
 @RequiredArgsConstructor
 public class UserRetrieveService {
+    private final UserRepository userRepository;
+
+    public UserDetailResponseDto getUserDetail(final Long userId) {
+        final User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION));
+        return new UserDetailResponseDto(user.getId(), user.getProfileImgUrl(), user.getName(), user.getRole());
+    }
 }
