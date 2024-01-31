530fa5d38fd69f0f961df40cc7e6083c1be63782
hellozo0 <ahju0011@gmail.com>
2024-01-31 17:42:01 +0900

    #219 [refactor] 모델 -> 제안서 Controller로 이동

---
+++ src/main/java/com/moddy/server/controller/offer/OfferController.java
@@ -0,0 +1,43 @@
+package com.moddy.server.controller.offer;
+
+import com.moddy.server.common.dto.ErrorResponse;
+import com.moddy.server.common.dto.SuccessResponse;
+import com.moddy.server.common.exception.enums.SuccessCode;
+import com.moddy.server.config.resolver.user.UserId;
+import com.moddy.server.controller.offer.dto.response.ModelMainOfferResponse;
+import com.moddy.server.service.offer.HairServiceOfferRetrieveService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OfferController {
+
+    private final HairServiceOfferRetrieveService hairServiceOfferRetrieveService;
+
+    @Tag(name = "ModelController")
+    @Operation(summary = "[JWT] 모델 메인 뷰 조회", description = "모델 메인 뷰 조회 API입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "모델 메인뷰 조회 성공", content = @Content(schema = @Schema(implementation = ModelMainOfferResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    @GetMapping("/model")
+    @SecurityRequirement(name = "JWT Auth")
+    public SuccessResponse<ModelMainOfferResponse> getModelMainInfo(
+            @Parameter(hidden = true) @UserId Long userId,
+            @Parameter(name = "page", description = "페이지 ") @RequestParam(value = "page") int page,
+            @Parameter(name = "size", description = "페이지 ") @RequestParam(value = "size") int size) {
+        return SuccessResponse.success(SuccessCode.FIND_MODEL_MAIN_INFO_SUCCESS, hairServiceOfferRetrieveService.getModelMainOfferInfo(userId, page, size));
+    }
+}
