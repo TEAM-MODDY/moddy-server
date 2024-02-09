ec148b91b6a79686b0481041e1980d6aa3d8a210
pkl0912 <pklpkl0912@gmail.com>
2024-02-10 01:06:26 +0900

    #236 [refactor] swagger 오류 수정

--- src/main/java/com/moddy/server/controller/designer/DesignerController.java
+++ src/main/java/com/moddy/server/controller/designer/DesignerController.java
@@ -9,10 +9,8 @@ import com.moddy.server.controller.designer.dto.request.OfferImageUrlRequestDto;
 import com.moddy.server.controller.designer.dto.response.ApplicationDetailInfoResponse;
 import com.moddy.server.controller.designer.dto.response.DownloadUrlResponseDto;
 import com.moddy.server.controller.designer.dto.response.UserCreateResponse;
-import com.moddy.server.service.application.HairModelApplicationRetrieveService;
 import com.moddy.server.service.designer.DesignerRegisterService;
 import com.moddy.server.service.designer.DesignerService;
-import com.moddy.server.service.offer.HairServiceOfferRegisterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,7 +24,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,8 +31,6 @@ import org.springframework.web.multipart.MultipartFile;
 import static com.moddy.server.common.exception.enums.SuccessCode.GET_PRE_SIGNED_URL_SUCCESS;
 
 @RestController
-@RequestMapping("/designer")
-@Tag(name = "DesignerController")
 @RequiredArgsConstructor
 public class DesignerController {
 
@@ -57,13 +52,14 @@ public class DesignerController {
         return SuccessResponse.success(SuccessCode.DESIGNER_CREATE_SUCCESS, designerRegisterService.createDesigner(designerId, designerInfo, profileImg));
     }
 
+    @Tag(name = "DesignerController")
     @Operation(summary = "[JWT] 제안서 다운로드 링크", description = "디자이너 제안서 다운로드 링크 불러오는 API")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "모델 지원서 상세 조회 성공", content = @Content(schema = @Schema(implementation = ApplicationDetailInfoResponse.class))),
             @ApiResponse(responseCode = "404", description = "해당 디자이너는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
-    @PostMapping("/offer/download-url")
+    @PostMapping("/designer/offer/download-url")
     @SecurityRequirement(name = "JWT Auth")
     public SuccessResponse<DownloadUrlResponseDto> getOfferImageDownloadUrl(
             @Parameter(hidden = true) @UserId Long userId,
