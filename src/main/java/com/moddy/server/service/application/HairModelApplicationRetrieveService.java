3930717a93f63520011a09a01a487fdfc03406d3
KWY <wodrk1001@gmail.com>
2024-02-14 17:22:14 +0900

    #233 [feat] 지원서 다운로드 url 조회 서비스 로직 구현

--- src/main/java/com/moddy/server/service/application/HairModelApplicationRetrieveService.java
+++ src/main/java/com/moddy/server/service/application/HairModelApplicationRetrieveService.java
@@ -3,6 +3,7 @@ package com.moddy.server.service.application;
 import com.moddy.server.common.exception.enums.ErrorCode;
 import com.moddy.server.common.exception.model.NotFoundException;
 import com.moddy.server.controller.designer.dto.response.DesignerMainResponse;
+import com.moddy.server.controller.designer.dto.response.DownloadUrlResponseDto;
 import com.moddy.server.controller.designer.dto.response.HairModelApplicationResponse;
 import com.moddy.server.controller.designer.dto.response.HairRecordResponse;
 import com.moddy.server.controller.model.dto.ApplicationDto;
@@ -15,6 +16,7 @@ import com.moddy.server.domain.hair_service_record.repository.HairServiceRecordJ
 import com.moddy.server.domain.prefer_hair_style.PreferHairStyle;
 import com.moddy.server.domain.prefer_hair_style.repository.PreferHairStyleJpaRepository;
 import com.moddy.server.domain.prefer_region.repository.PreferRegionJpaRepository;
+import com.moddy.server.external.s3.S3Service;
 import com.moddy.server.service.designer.DesignerRetrieveService;
 import com.moddy.server.service.model.ModelRetrieveService;
 import lombok.RequiredArgsConstructor;
@@ -36,6 +38,7 @@ public class HairModelApplicationRetrieveService {
     private final HairModelApplicationJpaRepository hairModelApplicationJpaRepository;
     private final DesignerRetrieveService designerRetrieveService;
     private final ModelRetrieveService modelRetrieveService;
+    private final S3Service s3Service;
     private final PreferHairStyleJpaRepository preferHairStyleJpaRepository;
     private final HairServiceRecordJpaRepository hairServiceRecordJpaRepository;
 
@@ -59,7 +62,8 @@ public class HairModelApplicationRetrieveService {
                 applicationResponsesList
         );
     }
-    public ApplicationDto getApplicationDetailInfo(final Long applicationId){
+
+    public ApplicationDto getApplicationDetailInfo(final Long applicationId) {
         HairModelApplication hairModelApplication = hairModelApplicationJpaRepository.findById(applicationId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_APPLICATION_EXCEPTION));
         Long modelId = hairModelApplication.getModel().getId();
         List<PreferHairStyle> preferHairStyles = preferHairStyleJpaRepository.findAllByHairModelApplicationId(applicationId);
@@ -87,6 +91,7 @@ public class HairModelApplicationRetrieveService {
                 hairModelApplication.getHairDetail(),
                 hairModelApplication.getInstagramId());
     }
+
     public String fetchApplicationHairDetail(final Long applicationId) {
         HairModelApplication hairModelApplication = hairModelApplicationJpaRepository.findById(applicationId).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_APPLICATION_EXCEPTION));
         return hairModelApplication.getHairDetail();
@@ -102,7 +107,7 @@ public class HairModelApplicationRetrieveService {
     }
 
 
-    public boolean fetchModelApplyStatus(final Long modelId){
+    public boolean fetchModelApplyStatus(final Long modelId) {
         return hairModelApplicationJpaRepository.existsByModelId(modelId);
     }
 
@@ -111,6 +116,13 @@ public class HairModelApplicationRetrieveService {
         Page<HairModelApplication> applicationPage = hairModelApplicationJpaRepository.findAll(pageRequest);
 
         return applicationPage;
+    }
+
+    public DownloadUrlResponseDto getApplicationCaptureDownloadUrl(final Long applicationId) {
+        final HairModelApplication hairModelApplication = hairModelApplicationJpaRepository.findById(applicationId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_APPLICATION_EXCEPTION));
+        final String applicationDownloadUrl = s3Service.getPreSignedUrlToDownload(hairModelApplication.getApplicationCaptureUrl());
+        return new DownloadUrlResponseDto(applicationDownloadUrl);
     }
 
     private HairModelApplicationResponse getApplicationResponse(final HairModelApplication application) {
