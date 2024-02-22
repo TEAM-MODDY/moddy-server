843c1d18cfb554ab6dfbc58d1f87a114cccdd122
hellozo0 <ahju0011@gmail.com>
2024-02-23 00:05:32 +0900

    #250 [refactor] Application Controller 조회/등록 컨트롤러 2가지로 분리

--- src/main/java/com/moddy/server/controller/application/ApplicationRetrieveController.java
+++ src/main/java/com/moddy/server/controller/application/ApplicationRetrieveController.java
@@ -1,7 +1,6 @@
 package com.moddy.server.controller.application;
 
 import com.moddy.server.common.dto.ErrorResponse;
-import com.moddy.server.common.dto.SuccessNonDataResponse;
 import com.moddy.server.common.dto.SuccessResponse;
 import com.moddy.server.common.exception.enums.SuccessCode;
 import com.moddy.server.config.resolver.user.UserId;
@@ -11,8 +10,6 @@ import com.moddy.server.controller.designer.dto.response.DesignerMainResponse;
 import com.moddy.server.controller.designer.dto.response.ModelInfoResponse;
 import com.moddy.server.controller.model.dto.ApplicationDto;
 import com.moddy.server.controller.model.dto.ApplicationModelInfoDto;
-import com.moddy.server.controller.model.dto.request.ModelApplicationRequest;
-import com.moddy.server.service.application.HairModelApplicationRegisterService;
 import com.moddy.server.service.application.HairModelApplicationRetrieveService;
 import com.moddy.server.service.model.ModelRetrieveService;
 import com.moddy.server.service.offer.HairServiceOfferRetrieveService;
@@ -24,45 +21,22 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Application Controller")
 @RequestMapping("/application")
-public class ApplicationController {
+public class ApplicationRetrieveController {
 
-    private final HairModelApplicationRegisterService hairModelApplicationRegisterService;
     private final HairModelApplicationRetrieveService hairModelApplicationRetrieveService;
     private final ModelRetrieveService modelRetrieveService;
     private final HairServiceOfferRetrieveService hairServiceOfferRetrieveService;
-
-    @Operation(summary = "[JWT] 모델 지원서 작성", description = "모델 지원서 작성 API입니다.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "모델 지원서 작성 성공"),
-            @ApiResponse(responseCode = "400", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-    })
-    @SecurityRequirement(name = "JWT Auth")
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public SuccessNonDataResponse submitModelApplication(
-            @Parameter(hidden = true) @UserId Long modelId,
-            @RequestPart(value = "modelImgUrl", required = false) MultipartFile modelImgUrl,
-            @RequestPart(value = "applicationCaptureImgUrl", required = false) MultipartFile applicationCaptureImgUrl,
-            @RequestPart(value = "applicationInfo") @Valid ModelApplicationRequest applicationInfo) {
-        hairModelApplicationRegisterService.postApplication(modelId, modelImgUrl, applicationCaptureImgUrl, applicationInfo);
-        return SuccessNonDataResponse.success(SuccessCode.CREATE_MODEL_APPLICATION_SUCCESS);
-    }
 
     @Operation(summary = "[JWT] 디자이너 메인 뷰 조회", description = "디자이너 메인 뷰 조회 API입니다.")
     @ApiResponses({
@@ -112,8 +86,7 @@ public class ApplicationController {
                 applicationDto.instgramId()
         );
 
-        ApplicationDetailInfoResponse applicationDetailInfoResponse = new ApplicationDetailInfoResponse(applicationInfoResponse,modelInfoResponse);
+        ApplicationDetailInfoResponse applicationDetailInfoResponse = new ApplicationDetailInfoResponse(applicationInfoResponse, modelInfoResponse);
         return SuccessResponse.success(SuccessCode.MODEL_APPLICATION_DETAil_INFO_SUCCESS, applicationDetailInfoResponse);
     }
 }
-
