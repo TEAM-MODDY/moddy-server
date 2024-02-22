5a23713a755d623ac532cb96e40077e00a3eb261
hellozo0 <ahju0011@gmail.com>
2024-02-22 15:30:14 +0900

    #250 [refactor] OfferController 스웨거 Tag 변경,라우터 변경

--- src/main/java/com/moddy/server/controller/offer/OfferController.java
+++ src/main/java/com/moddy/server/controller/offer/OfferController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,27 +34,13 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Offer Controller")
+@RequestMapping("/offer")
 public class OfferController {
 
     private final HairServiceOfferRetrieveService hairServiceOfferRetrieveService;
     private final HairServiceOfferRegisterService hairServiceOfferRegisterService;
 
-    @Tag(name = "ModelController")
-    @Operation(summary = "[JWT] 카카오톡 오픈채팅", description = "지원서 캡처 이미지 및 디자이너 정보 조회입니다")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "모델 메인뷰 조회 성공", content = @Content(schema = @Schema(implementation = OpenChatResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-    })
-    @GetMapping("/model/{offerId}/agree")
-    @SecurityRequirement(name = "JWT Auth")
-    public SuccessResponse<OpenChatResponse> getOpenChat(
-            @Parameter(hidden = true) @UserId Long userId,
-            @Parameter(name = "offerId", description = "제안서아이디") @PathVariable(value = "offerId") Long offerId) {
-        return SuccessResponse.success(SuccessCode.OPEN_CHAT_GET_SUCCESS, hairServiceOfferRetrieveService.getOpenChatInfo(userId, offerId));
-    }
-
-    @Tag(name = "ModelController")
     @Operation(summary = "[JWT] 디자이너 제안서 승낙하기", description = "디자이너 제안서 승낙하기 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "디자이너 제안서 승낙하기"),
@@ -61,7 +48,7 @@ public class OfferController {
             @ApiResponse(responseCode = "404", description = "제안서 아이디가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
-    @PutMapping("/model/offer/{offerId}")
+    @PutMapping("/{offerId}/agree")
     @SecurityRequirement(name = "JWT Auth")
     public SuccessNonDataResponse acceptOffer(
             @Parameter(hidden = true) @UserId Long userId,
@@ -70,14 +57,13 @@ public class OfferController {
         return SuccessNonDataResponse.success(SuccessCode.OFFER_ACCEPT_SUCCESS);
     }
 
-    @Tag(name = "ModelController")
     @Operation(summary = "[JWT] 모델 메인 뷰 조회", description = "모델 메인 뷰 조회 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "모델 메인뷰 조회 성공", content = @Content(schema = @Schema(implementation = ModelMainOfferResponse.class))),
             @ApiResponse(responseCode = "401", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
-    @GetMapping("/model")
+    @GetMapping()
     @SecurityRequirement(name = "JWT Auth")
     public SuccessResponse<ModelMainOfferResponse> getModelMainInfo(
             @Parameter(hidden = true) @UserId Long modelId,
@@ -86,7 +72,6 @@ public class OfferController {
         return SuccessResponse.success(SuccessCode.FIND_MODEL_MAIN_INFO_SUCCESS, hairServiceOfferRetrieveService.getModelMainOfferInfo(modelId, page, size));
     }
 
-    @Tag(name = "ModelController")
     @Operation(summary = "[JWT] 모델에게 온 디자이너 제안 상세보기", description = "제안서 상세보기 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "401", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
@@ -94,7 +79,7 @@ public class OfferController {
             @ApiResponse(responseCode = "404", description = "제안서 아이디가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
-    @GetMapping("/model/offer/{offerId}")
+    @GetMapping("/{offerId}")
     @SecurityRequirement(name = "JWT Auth")
     public SuccessResponse<DetailOfferResponse> getModelDetailOfferInfo(
             @Parameter(hidden = true) @UserId Long modelId,
@@ -102,7 +87,6 @@ public class OfferController {
         return SuccessResponse.success(SuccessCode.FIND_MODEL_DETAIL_OFFER_SUCCESS, hairServiceOfferRetrieveService.getOfferDetail(modelId, offerId));
     }
 
-    @Tag(name = "DesignerController")
     @Operation(summary = "[JWT] 제안서 작성하기", description = "제안서 작성하기 API입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "제안서 작성 성공", content = @Content(schema = @Schema(implementation = SuccessNonDataResponse.class))),
@@ -111,7 +95,7 @@ public class OfferController {
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
     @SecurityRequirement(name = "JWT Auth")
-    @PostMapping("/designer/{applicationId}/offer")
+    @PostMapping("/{applicationId}")
     public SuccessNonDataResponse offerCreateRequest(
             @Parameter(hidden = true) @UserId Long designerId,
             @PathVariable(value = "applicationId") Long applicationId,
