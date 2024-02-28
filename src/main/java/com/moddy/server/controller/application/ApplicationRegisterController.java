0543be46f2419c6a37a50eb2fe48969beb26a194
hellozo0 <ahju0011@gmail.com>
2024-02-28 22:29:54 +0900

    #260 [feat] Post API에 응답 data 추가

--- src/main/java/com/moddy/server/controller/application/ApplicationRegisterController.java
+++ src/main/java/com/moddy/server/controller/application/ApplicationRegisterController.java
@@ -1,9 +1,10 @@
 package com.moddy.server.controller.application;
 
 import com.moddy.server.common.dto.ErrorResponse;
-import com.moddy.server.common.dto.SuccessNonDataResponse;
+import com.moddy.server.common.dto.SuccessResponse;
 import com.moddy.server.common.exception.enums.SuccessCode;
 import com.moddy.server.config.resolver.user.UserId;
+import com.moddy.server.controller.application.dto.response.ApplicationExpireDateResponse;
 import com.moddy.server.controller.model.dto.request.ModelApplicationRequest;
 import com.moddy.server.service.application.HairModelApplicationRegisterService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -33,18 +34,17 @@ public class ApplicationRegisterController {
 
     @Operation(summary = "[JWT] 모델 지원서 작성", description = "모델 지원서 작성 API입니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "모델 지원서 작성 성공"),
+            @ApiResponse(responseCode = "200", description = "모델 지원서 작성 성공", content = @Content(schema = @Schema(implementation = ApplicationExpireDateResponse.class))),
             @ApiResponse(responseCode = "400", description = "인증 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류 입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
     })
     @SecurityRequirement(name = "JWT Auth")
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public SuccessNonDataResponse submitModelApplication(
+    public SuccessResponse<ApplicationExpireDateResponse> submitModelApplication(
             @Parameter(hidden = true) @UserId Long modelId,
             @RequestPart(value = "modelImgUrl", required = false) MultipartFile modelImgUrl,
             @RequestPart(value = "applicationCaptureImgUrl", required = false) MultipartFile applicationCaptureImgUrl,
             @RequestPart(value = "applicationInfo") @Valid ModelApplicationRequest applicationInfo) {
-        hairModelApplicationRegisterService.postApplication(modelId, modelImgUrl, applicationCaptureImgUrl, applicationInfo);
-        return SuccessNonDataResponse.success(SuccessCode.CREATE_MODEL_APPLICATION_SUCCESS);
+        return SuccessResponse.success(SuccessCode.CREATE_MODEL_APPLICATION_SUCCESS, hairModelApplicationRegisterService.postApplication(modelId, modelImgUrl, applicationCaptureImgUrl, applicationInfo));
     }
 }
